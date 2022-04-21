23ea5e9e6b2481830df4e47604b196045d545a56
ahyoon99 <kimay9623@naver.com>
2022-04-21 17:52:14 +0900

    swagger 설정하기 - @ApiParam

--- src/main/java/com/example/swagger/controller/ApiController.java
+++ src/main/java/com/example/swagger/controller/ApiController.java
@@ -1,6 +1,7 @@
 package com.example.swagger.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.*;
 
 @Api(tags = {"API 정보를 제공하는 Controller"})    // 1. @Api
@@ -12,4 +13,15 @@ public class ApiController {
     public String hello(){
         return "hello";
     }
+
+    @GetMapping("/plus/{x}")
+    public int plus(
+            @ApiParam(value="x값")   // 2. @ApiParam
+            @PathVariable int x,
+
+            @ApiParam(value="y값")
+            @RequestParam int y){
+        return x+y;
+    }
+
 }
