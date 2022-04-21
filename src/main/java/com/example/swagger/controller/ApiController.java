251dcff51a1cd8598f30ed59d19ce605d01301fc
ahyoon99 <kimay9623@naver.com>
2022-04-21 23:36:19 +0900

    swagger 설정하기 - @ApiResponse

--- src/main/java/com/example/swagger/controller/ApiController.java
+++ src/main/java/com/example/swagger/controller/ApiController.java
@@ -5,7 +5,9 @@ import com.example.swagger.dto.UserRes;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.service.RequestBody;
 
 @Api(tags = {"API 정보를 제공하는 Controller"})    // 1. @Api
 @RestController
@@ -27,6 +29,7 @@ public class ApiController {
         return x+y;
     }
 
+    @ApiResponse(code = 502, message = "사용자의 나이가 10살 이하일 때")
     @ApiOperation(value = "사용자의 이름과 나이를 리턴하는 메소드")
     @GetMapping("/user")
     public UserRes user(UserReq userReq){
